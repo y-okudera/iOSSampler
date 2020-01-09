0f123318bce421421cbb2e59ec3db91ca3031d2f
Yuki Okudera <>
2020-01-09 10:52:34 +0900

    Create a new project for iOS.

---
+++ iOSSampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSSampler
+//
+//  Created by Yuki Okudera on 2020/01/09.
+//  Copyright © 2020 YukiOkudera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
